952cb3d6b12f85b3c20258d4668fd684f0206421
Melso <melsopv@gmail.com>
2024-11-26 19:17:09 +0100

    hehexd my bad again v2

--- srcs/requirements/nginx/conf/entry.sh
+++ srcs/requirements/nginx/conf/entry.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Use envsubst to substitute environment variables into nginx config template
-envsubst '${SERVER_NAME} ${SSL_CERTIFICATE} ${SSL_CERTIFICATE_KEY}' < /etc/nginx/http.d/nginx.conf.template > /etc/nginx/http.d/nginx.conf
+# Use envsubst to substitute environment variables directly in the existing nginx.conf
+envsubst '${SERVER_NAME} ${SSL_CERTIFICATE} ${SSL_CERTIFICATE_KEY}' < /etc/nginx/http.d/nginx.conf > /etc/nginx/http.d/nginx.conf.tmp
+
+# Replace the original nginx.conf with the updated version
+mv /etc/nginx/http.d/nginx.conf.tmp /etc/nginx/http.d/nginx.conf
 
 # Start Nginx
 exec "$@"
